ee8dede0b6db6d134daad1b66f8c042b1042e4a8
nikitahaliked04 <56929861+nikitahaliked04@users.noreply.github.com>
2022-02-23 19:29:31 +0530

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,16 @@
+import turtle
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(144)
+turtle.penup()
+turtle.goto(200, 100)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(144)
+turtle.penup()
+turtle.goto(10, 100)
+turtle.pendown()
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(144)
